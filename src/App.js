4e4093bafed976cd820ef9c3c2d2a54096f2a2f8
Marcus Jones <indelibleink84@gmail.com>
2019-07-22 19:14:01 -0500

    complete: updated: Contact, Home, About and Navication js.d

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 
-const App = () => (
-  <div>
-    <Navigation />
-  </div>
-);
+
+import './App.css';
+import { Home, About, Contact, Navigation } from './components';  
+
+
+const App = () =>{  
+  return (
+<div className ='App'>
+  <div className = 'main-nav-container'>
+    <nav>
+      <ul>
+      <li>
+          <Link to = '/'>Home</Link>
+        </li>
+        <li>
+          <Link to = '/Navigation/'>Navigation </Link>
+        </li>
+        <li>
+          <Link to = '/Contact/'>Contact </Link>
+        </li>
+        <li>
+          <Link to= '/About/'>About </Link>
+        </li>
+      </ul>
+    </nav>
+      <Switch>
+        <Route path ='/' exact component = {Home} />
+        <Route path ='/About' exact component = {About} />
+        <Route path ='/contact' exact component ={Contact} />
+        <Route path = '/Navigation' exact component ={Navigation} />
+      </Switch>
+    </div>
+</div>
+    );
+  }
 
 export default App;
--- src/components/About.js
+++ src/components/About.js
@@ -3,3 +3,4 @@ import React from 'react';
 const About = () => <h1>About</h1>;
 
 export default About;
+
